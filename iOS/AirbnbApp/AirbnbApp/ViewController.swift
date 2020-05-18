53078fdebcd71947bf32424a58dd15f008942128
corykim0829 <corykim0829@gmail.com>
2020-05-18 18:41:58 +0900

    feat: init project
    프로젝트 생성
    
    issue: #1

---
+++ iOS/AirbnbApp/AirbnbApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirbnbApp
+//
+//  Created by Cory Kim on 2020/05/18.
+//  Copyright © 2020 codesquad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
