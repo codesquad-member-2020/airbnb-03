d6c6c46f29c4528454850e9a75ee4c12046f1e8c
haveagood@naver.com <haveagood@naver.com>
2020-05-18 16:28:35 +0900

    [chore] dev branch 생성 및 be 프로젝트 폴더 생성

---
+++ be/src/main/java/com/airbnb3/codesquad/airbnb3/Airbnb3Application.java
@@ -0,0 +1,13 @@
+package com.airbnb3.codesquad.airbnb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Airbnb3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Airbnb3Application.class, args);
+    }
+
+}
