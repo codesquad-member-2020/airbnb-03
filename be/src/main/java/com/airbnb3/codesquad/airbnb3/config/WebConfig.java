c4fdb6be5bea8de165efee03118bb4ab7b19aaf4
haveagood@naver.com <haveagood@naver.com>
2020-05-18 21:26:50 +0900

    [BE] feat : CORS에러 방지를 위한 filter와 webConfig 추가
    - CORS 에러 방지를 위해서 CORS filter, WebConfig 추가

---
+++ be/src/main/java/com/airbnb3/codesquad/airbnb3/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.airbnb3.codesquad.airbnb3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
