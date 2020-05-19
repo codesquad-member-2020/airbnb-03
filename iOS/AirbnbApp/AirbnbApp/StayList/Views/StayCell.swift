aa1c3ae845df00abd0517af06e233805095dae36
corykim0829 <corykim0829@gmail.com>
2020-05-19 23:55:46 +0900

    [iOS] feat: StayCell XIB 구현
    전체적인 UI Layout 구현, nibName 프로퍼티
    
    issue: #15

---
+++ iOS/AirbnbApp/AirbnbApp/StayList/Views/StayCell.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class StayCell: UICollectionViewCell {
+    
+    static let nibName: String = String(describing: StayCell.self)
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
