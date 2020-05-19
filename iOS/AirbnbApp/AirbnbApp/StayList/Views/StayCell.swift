40cacad908e56d3e19761be230d5f37ad7037f68
corykim0829 <corykim0829@gmail.com>
2020-05-20 00:07:50 +0900

    [iOS] feat: StayCell reuseIdentifier 추가 및 collectionView에 등록
    issue: #15

--- iOS/AirbnbApp/AirbnbApp/StayList/Views/StayCell.swift
+++ iOS/AirbnbApp/AirbnbApp/StayList/Views/StayCell.swift
@@ -3,6 +3,7 @@ import UIKit
 class StayCell: UICollectionViewCell {
     
     static let nibName: String = String(describing: StayCell.self)
+    static let reuseIdentifier: String = "StayCell"
 
     override func awakeFromNib() {
         super.awakeFromNib()
