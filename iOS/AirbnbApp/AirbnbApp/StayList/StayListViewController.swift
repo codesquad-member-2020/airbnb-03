ce70f08b99d39b35858baabe35d625e63eeed876
corykim0829 <corykim0829@gmail.com>
2020-05-19 17:59:54 +0900

    [iOS] feat: TabBarController viewControllers 설정
    issue: #1

--- iOS/AirbnbApp/AirbnbApp/StayList/StayListViewController.swift
+++ iOS/AirbnbApp/AirbnbApp/StayList/StayListViewController.swift
@@ -4,5 +4,11 @@ class StayListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
     }
 }
--- iOS/AirbnbApp/AirbnbApp/TabBar/TabBarController.swift
+++ iOS/AirbnbApp/AirbnbApp/TabBar/TabBarController.swift
@@ -1,9 +1,19 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    
+    private var stayListViewController: StayListViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        configureTabBarControllers()
+    }
+    
+    private func configureTabBarControllers() {
+        stayListViewController = StayListViewController()
+        
+        viewControllers = [stayListViewController]
     }
 }
