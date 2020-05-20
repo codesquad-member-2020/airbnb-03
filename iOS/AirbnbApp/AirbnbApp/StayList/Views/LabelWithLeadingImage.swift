dcd463ebbe3cf4c25d88ed7bfec8580bca805aab
corykim0829 <corykim0829@gmail.com>
2020-05-21 03:27:52 +0900

    [iOS] feat: LabelWithLeadingImage 프로퍼티 추가
    image 별로 위치를 조금씩 변동 시켜줘야해서, Yconstant 변수를 추가하여 기본값을 설정해주었습니다.
    
    issue: #20

--- iOS/AirbnbApp/AirbnbApp/StayList/Views/LabelWithLeadingImage.swift
+++ iOS/AirbnbApp/AirbnbApp/StayList/Views/LabelWithLeadingImage.swift
@@ -12,6 +12,7 @@ protocol LabelWithLeadingImage: UILabel {
     var leadingImage: UIImage? { get set }
     var leadingImageTintColor: UIColor! { get }
     var imageAttachment: NSTextAttachment! { get set }
+    var leadingImageYConstant: CGFloat { get }
     
     func configureImageAttachment()
 }
@@ -21,10 +22,12 @@ extension LabelWithLeadingImage {
         UIColor(named: "stay.tintcolor")
     }
     
+    var leadingImageYConstant: CGFloat { 0 }
+    
     func configureImageAttachment() {
         self.imageAttachment = NSTextAttachment()
         let height = self.bounds.height * 0.8
-        imageAttachment.bounds = .init(x: 0, y: -(self.bounds.midY - height / 2), width: height, height: height)
+        imageAttachment.bounds = .init(x: 0, y: -(self.bounds.midY - height * 0.5) - leadingImageYConstant, width: height, height: height)
         imageAttachment.image = leadingImage?.withTintColor(leadingImageTintColor)
     }
 }
