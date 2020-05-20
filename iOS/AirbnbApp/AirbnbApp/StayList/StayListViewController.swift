e98e18488503480e12e9b851379569d8f9dd8a95
kjoonk <kjoonk@gmail.com>
2020-05-20 20:25:06 +0900

    [iOS] feat: Search Field 뷰 구현
    - xib 사용
    - VC에서 로딩
    
    issue: #18

--- iOS/AirbnbApp/AirbnbApp/StayList/StayListViewController.swift
+++ iOS/AirbnbApp/AirbnbApp/StayList/StayListViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class StayListViewController: UIViewController {
     
-    private var searchViewLayout: UIView!
+    private var searchView: SearchTextField!
     private var filterViewLayout: UIView!
     private var stayListCollectionView: StayListCollectionView!
     private var stayListCollectionViewDataSource: StayListCollectionViewDataSource!
@@ -22,24 +22,32 @@ class StayListViewController: UIViewController {
     
     private func configureUI() {
         view.backgroundColor = .white
-        
-        searchViewLayout = UIView()
-        searchViewLayout.backgroundColor = .red
+
+        searchView = viewFromNib(SearchTextField.self)
+
         filterViewLayout = UIView()
         filterViewLayout.backgroundColor = .blue
         stayListCollectionView = StayListCollectionView()
+    }
+
+    private func viewFromNib<T>(_ type: T.Type) -> T where T: ViewFromXib {
+        guard let view = Bundle.main.loadNibNamed(T.xibName, owner: nil, options: nil)?.first as? T else {
+            fatalError("Failed to load from xib: \(T.self)")
+        }
+
+        return view
     }
 }
 
 // MARK:- Layout
 extension StayListViewController {
     private func configureLayout() {
-        view.addSubviews(searchViewLayout,
+        view.addSubviews(searchView,
                          filterViewLayout,
                          stayListCollectionView)
         
         let sidePadding: CGFloat = 24.0
-        searchViewLayout.constraints(topAnchor: view.safeAreaLayoutGuide.topAnchor,
+        searchView.constraints(topAnchor: view.safeAreaLayoutGuide.topAnchor,
                                      leadingAnchor: view.leadingAnchor,
                                      bottomAnchor: nil, trailingAnchor: view.trailingAnchor,
                                      padding: .init(top: 16,
@@ -47,19 +55,19 @@ extension StayListViewController {
                                                     bottom: 0,
                                                     right: sidePadding),
                                      size: .init(width: 0, height: 48))
-        filterViewLayout.constraints(topAnchor: searchViewLayout.bottomAnchor,
-                                     leadingAnchor: searchViewLayout.leadingAnchor,
+        filterViewLayout.constraints(topAnchor: searchView.bottomAnchor,
+                                     leadingAnchor: searchView.leadingAnchor,
                                      bottomAnchor: nil,
-                                     trailingAnchor: searchViewLayout.trailingAnchor,
+                                     trailingAnchor: searchView.trailingAnchor,
                                      padding: .init(top: 8,
                                                     left: 0,
                                                     bottom: 0,
                                                     right: 0),
                                      size: .init(width: 0, height: 44))
         stayListCollectionView.constraints(topAnchor: filterViewLayout.bottomAnchor,
-                                         leadingAnchor: searchViewLayout.leadingAnchor,
+                                         leadingAnchor: searchView.leadingAnchor,
                                          bottomAnchor: view.safeAreaLayoutGuide.bottomAnchor,
-                                         trailingAnchor: searchViewLayout.trailingAnchor,
+                                         trailingAnchor: searchView.trailingAnchor,
                                          padding: .init(top: 12,
                                                         left: 0,
                                                         bottom: 0,
