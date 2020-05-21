a0badad15da7c9c94f4352e33b963ffce8507208
kjoonk <kjoonk@gmail.com>
2020-05-21 15:06:11 +0900

    [iOS] feat: 텍스트 필드 shadow 처리 변경
    issue: #18

--- iOS/AirbnbApp/AirbnbApp/StayList/Views/SearchFieldView.swift
+++ iOS/AirbnbApp/AirbnbApp/StayList/Views/SearchFieldView.swift
@@ -1,25 +1,12 @@
 import UIKit
 
-/// 검색 텍스트 필드에 관한 뷰
 class SearchFieldView: UIView, ViewFromXib {
     static let xibName = String(describing: SearchFieldView.self)
 
-    @IBOutlet weak var searchTextField: UITextField! {
-        didSet {
-            drawShadow(searchTextField)
-        }
-    }
+    @IBOutlet weak var searchTextField: SearchTextField!
 
     func configureTextFieldDelegate(_ delegate: UITextFieldDelegate) {
         searchTextField.delegate = delegate
-    }
-
-    private func drawShadow(_ subView: UIView) {
-        subView.layer.shadowColor = UIColor.black.cgColor
-        subView.layer.shadowOpacity = 0.6
-        subView.layer.shadowOffset = .zero
-        subView.layer.shadowRadius = 5
-        subView.layer.shouldRasterize = true
     }
 }
 
