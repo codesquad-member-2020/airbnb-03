04a5fcee83ce9a1e9fab944c8c49d188c1cced2c
corykim0829 <corykim0829@gmail.com>
2020-05-21 12:05:53 +0900

    [iOS] feat: PriceLabel 구현
    Int 가격값을 받아서 numberFormattter를 적용하여 화면에 표시하도록 구현하였습니다.
    
    issue: #20

--- iOS/AirbnbApp/AirbnbApp/StayList/Views/StayCell.swift
+++ iOS/AirbnbApp/AirbnbApp/StayList/Views/StayCell.swift
@@ -9,6 +9,7 @@ class StayCell: UICollectionViewCell {
     @IBOutlet weak var superHostLabel: SuperHostLabel!
     @IBOutlet weak var placeTypeAndCityLabel: PlaceTypeAndCityLabel!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: PriceLabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -16,5 +17,6 @@ class StayCell: UICollectionViewCell {
         reviewLabel.updateWith(reviewAverage: 4.67, numberOfReviews: 47)
         placeTypeAndCityLabel.updateWith(type: "Entire Apartment", city: "Upper East Side")
         titleLabel.text = "Modern luxury studio in Gangnam! 5sec to Station"
+        priceLabel.updateWith(price: 1890)
     }
 }
