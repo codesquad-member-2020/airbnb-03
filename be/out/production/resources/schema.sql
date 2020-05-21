d38e02a7ad509b1611acab7b3fae393e30bbd449
haveagood@naver.com <haveagood@naver.com>
2020-05-21 14:46:36 +0900

    [BE] chore : schema , data 수정 및 DATA INSERT query문 작성
    - schema 형식을 data 형식에 맞춰서 수정하였습니다.
    - init data 수정
    - data를 넣기위한 쿼리문 작성
    
    # 본문 작성
    
    # 꼬릿말은 아래에 작성 : ex) #이슈 번호
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

--- be/out/production/resources/schema.sql
+++ be/out/production/resources/schema.sql
@@ -7,76 +7,75 @@ drop table if exists images;
 
 create table if not exists properties
 (
-    id INT AUTO_INCREMENT,
-    title VARCHAR(64),
-    states VARCHAR(64),
-    cities VARCHAR(64),
-    address VARCHAR(128) DEFAULT 'Not Address',
-    zipcode INT,
-    latitude DOUBLE,
-    longitude DOUBLE,
-    reservable BOOLEAN,
-    super_host BOOLEAN,
-    host_type VARCHAR(32) DEFAULT 'Not Tag',
-    price DECIMAL(10,3),
-    place_type VARCHAR(32),
-    review_average DECIMAL(3,2),
+    id                INT,
+    title             VARCHAR(64),
+    state             VARCHAR(64),
+    city              VARCHAR(64),
+    address           VARCHAR(128) DEFAULT 'Not Address',
+    latitude          DOUBLE,
+    longitude         DOUBLE,
+    reservable        BOOLEAN,
+    saved             BOOLEAN,
+    host_type         VARCHAR(32)  DEFAULT 'Not Tag',
+    price             DECIMAL(10, 3),
+    place_type        VARCHAR(32),
+    review_average    DECIMAL(3, 2),
     number_of_reviews INT,
-    user_id INT REFERENCES user(id),
+    user_id           INT REFERENCES user (id),
     primary key (id)
 );
 
 create table if not exists bookings
 (
-    id INT AUTO_INCREMENT,
-    check_in_date DATETIME,
-    check_out_date DATETIME,
-    booking_date DATETIME,
-    cleaning_fee DECIMAL(10,3),
-    service_fee DECIMAL(10,3),
-    occupancy_tax_fee DECIMAL(10,3),
-    booking_price DECIMAL(10,3),
-    status BOOLEAN,
-    properties_id INT REFERENCES properties(id),
-    user_id INT REFERENCES user(id),
+    id                INT AUTO_INCREMENT,
+    check_in_date     DATETIME,
+    check_out_date    DATETIME,
+    booking_date      DATETIME,
+    cleaning_fee      DECIMAL(10, 3),
+    service_fee       DECIMAL(10, 3),
+    occupancy_tax_fee DECIMAL(10, 3),
+    booking_price     DECIMAL(10, 3),
+    status            BOOLEAN,
+    properties_id     INT REFERENCES properties (id),
+    user_id           INT REFERENCES user (id),
     primary key (id)
 );
 
 create table if not exists user
 (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(64),
+    id    INT AUTO_INCREMENT,
+    name  VARCHAR(64),
     email VARCHAR(128),
     primary key (id)
 );
 
 create table if not exists detail
 (
-    id INT AUTO_INCREMENT,
-    title VARCHAR(64),
-    description VARCHAR(64),
-    host VARCHAR(32),
+    id                INT AUTO_INCREMENT,
+    title             VARCHAR(64),
+    description       VARCHAR(64),
+    host              VARCHAR(32),
     total_guest_count INT,
-    bedroom_count INT,
-    bathroom_count INT,
+    bedroom_count     INT,
+    bathroom_count    INT,
     availability_type BOOLEAN,
-    properties_id INT REFERENCES properties(id),
+    properties_id     INT REFERENCES properties (id),
     primary key (id)
 );
 
 create table if not exists bookmarks
 (
-    id INT AUTO_INCREMENT,
+    id            INT AUTO_INCREMENT,
     is_bookmarked BOOLEAN,
-    user_id INT REFERENCES user(id),
-    properties_id INT REFERENCES properties(id),
+    user_id       INT REFERENCES user (id),
+    properties_id INT REFERENCES properties (id),
     primary key (id)
 );
 
 create table if not exists images
 (
-    id INT AUTO_INCREMENT,
-    image_url VARCHAR(128),
-    properties_id INT REFERENCES properties(id),
+    id            INT AUTO_INCREMENT,
+    image_url     VARCHAR(128),
+    properties_id INT REFERENCES properties (id),
     primary key (id)
 );
