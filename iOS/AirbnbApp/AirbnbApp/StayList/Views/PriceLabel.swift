4b63d9fb283819f757c38b1c078cfe3a0ce56dfc
corykim0829 <corykim0829@gmail.com>
2020-05-21 15:54:47 +0900

    [iOS] refactor: priceLabel 달러 사인 앞으로 이동
    issue: #20

--- iOS/AirbnbApp/AirbnbApp/StayList/Views/PriceLabel.swift
+++ iOS/AirbnbApp/AirbnbApp/StayList/Views/PriceLabel.swift
@@ -4,7 +4,7 @@ class PriceLabel: UILabel {
 
     func updateWith(price: Int) {
         let attributedText = NSMutableAttributedString(string: "")
-        attributedText.append(text: "\(price.priceFormat)$", fontSize: 17, weight: .bold)
+        attributedText.append(text: "$\(price.priceFormat)", fontSize: 17, weight: .bold)
         attributedText.append(text: " / night", fontSize: 17, weight: .light)
         self.attributedText = attributedText
     }
