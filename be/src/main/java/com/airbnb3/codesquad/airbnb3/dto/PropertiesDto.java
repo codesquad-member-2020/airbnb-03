a1df34605c61e81ae82e5f007dc2627b8aeeff11
haveagood@naver.com <haveagood@naver.com>
2020-05-21 17:10:44 +0900

    [BE] feat : 값을 저장하는 Dto 생성
    - Main Page 구현에 필요한 인자값을 담는 Dto 클래스를 생성하였습니다.
    
    - Lombok을 활용해서 Builder를 사용하였습니다.
    
    issue : #5

---
+++ be/src/main/java/com/airbnb3/codesquad/airbnb3/dto/PropertiesDto.java
@@ -0,0 +1,50 @@
+package com.airbnb3.codesquad.airbnb3.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class PropertiesDto {
+    private Long id;
+    private String title;
+    private String state;
+    private String city;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private Boolean reservable;
+    private Boolean saved;
+    private String hostType;
+    private Double price;
+    private String placeType;
+    private Double reviewAverage;
+    private Integer numberOfReviews;
+    private List<String> images;
+
+    @Builder
+    public PropertiesDto(Long id, String title, String state, String city, String address,
+                         Double latitude, Double longitude, Boolean reservable, Boolean saved,
+                         String hostType, Double price, String placeType, Double reviewAverage, Integer numberOfReviews, List<String> images) {
+        this.id = id;
+        this.title = title;
+        this.state = state;
+        this.city = city;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.reservable = reservable;
+        this.saved = saved;
+        this.hostType = hostType;
+        this.price = price;
+        this.placeType = placeType;
+        this.reviewAverage = reviewAverage;
+        this.numberOfReviews = numberOfReviews;
+        this.images = images;
+    }
+}
