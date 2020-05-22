d4a5f677d0160e141b9e63262b9de650e98c3eaa
corykim0829 <corykim0829@gmail.com>
2020-05-22 16:39:22 +0900

    [iOS] feat: StayCell 사이즈 변경
    비율에 맞게 계산하여 처리
    
    issue: #29

--- iOS/AirbnbApp/AirbnbApp/StayList/Views/StayListCollectionView.swift
+++ iOS/AirbnbApp/AirbnbApp/StayList/Views/StayListCollectionView.swift
@@ -32,8 +32,16 @@ class StayListCollectionView: UICollectionView {
     }
 }
 
-extension StayListCollectionView: UICollectionViewDelegateFlowLayout {
+extension StayListCollectionView: UICollectionViewDelegateFlowLayout {	
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        .init(width: collectionView.bounds.width, height: collectionView.bounds.height / 2)
+        let scrollViewHeight = collectionView.bounds.width * (3 / 5)
+        let verticalSpace: CGFloat = 12.0
+        let stackViewHeight: CGFloat = 100.0
+
+        return CGSize(width: collectionView.bounds.width, height: scrollViewHeight + verticalSpace + stackViewHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        24
     }
 }
