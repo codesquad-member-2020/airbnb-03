665c394d1604cf0ee440ee3f98d9da0db6f5ef9f
corykim0829 <corykim0829@gmail.com>
2020-05-22 15:15:04 +0900

    [iOS] feat: pageIndex 계산 round 처리 제거
    issue: #33

--- iOS/AirbnbApp/AirbnbApp/StayList/Controllers/ThumbScrollViewDelegate.swift
+++ iOS/AirbnbApp/AirbnbApp/StayList/Controllers/ThumbScrollViewDelegate.swift
@@ -2,6 +2,6 @@ import UIKit
 
 class ThumbScrollViewDelegate: NSObject, UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let pageIndex = Int(round(scrollView.contentOffset.x / scrollView.frame.width))
+        let pageIndex = Int(scrollView.contentOffset.x / scrollView.frame.width)
     }
 }
