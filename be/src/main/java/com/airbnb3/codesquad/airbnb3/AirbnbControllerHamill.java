1f61d2df3765ed9384162ffa313996465565e978
Hamill210 <han95210@naver.com>
2020-05-25 11:36:49 +0900

    [BE] modify : class 이름 변경

--- be/src/main/java/com/airbnb3/codesquad/airbnb3/AirbnbControllerHamill.java
+++ be/src/main/java/com/airbnb3/codesquad/airbnb3/AirbnbControllerHamill.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AirbnbController {
+public class AirbnbControllerHamill {
 
     @GetMapping("/index")
     public String index() {
