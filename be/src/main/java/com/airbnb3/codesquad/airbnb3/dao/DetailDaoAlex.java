e158e33498da5e8826385d70d5cc9fbfcc675ec9
haveagood@naver.com <haveagood@naver.com>
2020-05-27 00:52:32 +0900

    [BE] refactor : amenities column 쿼리 수정
    - amenities 데이터가 ""로 이루어져있어 DB에서 가져올때, 깔끔하게
      가져와지지 않았다.
    - REPLACE를 활용하여 Data를 정리해서 가져오도록 변환
    
    issue : #7
    
    -------------------------
    -------------------------

--- be/src/main/java/com/airbnb3/codesquad/airbnb3/dao/DetailDaoAlex.java
+++ be/src/main/java/com/airbnb3/codesquad/airbnb3/dao/DetailDaoAlex.java
@@ -22,7 +22,7 @@ public class DetailDaoAlex {
 
     public DetailDtoAlex getDetailProperties(Long id) {
         String sql = "SELECT p.id,p.city,p.state,p.title,p.state,p.city,p.latitude,p.longitude,p.reservable,p.saved,CASE p.host_type WHEN 'super' THEN 1 ELSE 0 END AS is_super_host,p.price,p.place_type,p.review_average,p.number_of_reviews,GROUP_CONCAT(i.image_url) AS image," +
-                "d.summary,d.space,d.city_overview,d.notes,d.transit,d.host_name,d.host_since,d.host_location,d.host_about,d.address,d.accommodates,d.bathrooms,d.bedrooms,d.beds,d.bed_type,d.amenities,d.service_fee,d.cleaning_fee,d.tax,d.review_scores_accuracy,d.review_scores_cleanliness,d.review_scores_checkin,d.review_scores_communication,d.review_scores_location,d.review_scores_value " +
+                "d.summary,d.space,d.city_overview,d.notes,d.transit,d.host_name,d.host_since,d.host_location,d.host_about,d.address,d.accommodates,d.bathrooms,d.bedrooms,d.beds,d.bed_type,REPLACE(d.amenities,'\"','') AS amenity,d.service_fee,d.cleaning_fee,d.tax,d.review_scores_accuracy,d.review_scores_cleanliness,d.review_scores_checkin,d.review_scores_communication,d.review_scores_location,d.review_scores_value " +
                 "FROM properties p LEFT JOIN detail d ON p.id = d.id LEFT JOIN images i ON p.id = i.properties_id WHERE p.id = :id";
 
         MapSqlParameterSource parameterSource = new MapSqlParameterSource();
@@ -70,7 +70,7 @@ public class DetailDaoAlex {
                                 .build())
                         .roomInfo(RoomDto.builder()
                                 .accommodates(rs.getInt("accommodates"))
-                                .amenities(rs.getString("amenities"))
+                                .amenities(rs.getString("amenity"))
                                 .bathrooms(rs.getInt("bathrooms"))
                                 .bedrooms(rs.getInt("bedrooms"))
                                 .beds(rs.getInt("beds"))
@@ -81,46 +81,4 @@ public class DetailDaoAlex {
                                 .build())
                         .build());
     }
-
-
-    //DetailDtoAlex.builder()
-    //                .id(rs.getLong("id"))
-    //                .title(rs.getString("title"))
-    //                .address(rs.getString("address"))
-    //                .latitude(rs.getDouble("latitude"))
-    //                .longitude(rs.getDouble("longitude"))
-    //                .reservable(rs.getBoolean("reservable"))
-    //                .saved(rs.getBoolean("saved"))
-    //                .hostType(rs.getString("host_type"))
-    //                .price(rs.getDouble("price"))
-    //                .serviceFee(rs.getDouble("service_fee"))
-    //                .cleaningFee(rs.getDouble("cleaning_fee"))
-    //                .tax(rs.getDouble("tax"))
-    //                .placeType(rs.getString("place_type"))
-    //                .reviewAverage(rs.getDouble("review_average"))
-    //                .numberOfReviews(rs.getInt("number_of_reviews"))
-    //                .images(Arrays.asList(rs.getString("image").split(",")))
-    //                .summary(rs.getString("summary"))
-    //                .space(rs.getString("space"))
-    //                .cityOverView(rs.getString("city_over_view"))
-    //                .notes(rs.getString("notes"))
-    //                .transit(rs.getString("transit"))
-    //                .hostName(rs.getString("host_name"))
-    //                .hostSince(Date.valueOf(rs.getString("host_since")))
-    //                .hostLocation(rs.getString("host_location"))
-    //                .hostAbout(rs.getString("host_about"))
-    //                .accommodates(rs.getInt("accommodates"))
-    //                .bathrooms(rs.getInt("bathrooms"))
-    //                .bedrooms(rs.getInt("bedrooms"))
-    //                .beds(rs.getInt("beds"))
-    //                .bedType(rs.getString("bed_type"))
-    //                .amenities(rs.getString("amenities"))
-    //                .reviewScoresAccuracy(format.format(rs.getDouble("review_scores_accuracy")))
-    //                .reviewScoresCleanliness(format.format(rs.getDouble("review_scores_cleanliness")))
-    //                .reviewScoresCheckin(format.format(rs.getDouble("review_scores_checkin")))
-    //                .reviewScoresCommunication(format.format(rs.getDouble("review_scores_communication")))
-    //                .reviewScoresLocation(format.format(rs.getDouble("review_scores_location")))
-    //                .reviewScoresValue(format.format(rs.getDouble("review_scores_value")))
-    //                .city(rs.getString("city"))
-    //                .state(rs.getString("state"))
 }
--- be/src/main/java/com/airbnb3/codesquad/airbnb3/dto/PropertiesDtoAlex.java
+++ be/src/main/java/com/airbnb3/codesquad/airbnb3/dto/PropertiesDtoAlex.java
@@ -7,7 +7,6 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor
 public class PropertiesDtoAlex {
 
     private Long id;
