538c9ccb003d46861fba11da698c67f54f9f07f1
Hamill210 <han95210@naver.com>
2020-05-27 18:03:14 +0900

    [BE] modify : hamill 환경에 맞게 속성값과 data.sql 변경

--- be/out/production/resources/data.sql
+++ be/out/production/resources/data.sql
@@ -1,24 +1,24 @@
 LOAD DATA INFILE 'properties.csv'
-INTO TABLE airbnb_a.properties
+INTO TABLE airbnb.properties
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (id,host_type,title,city,state,latitude,longitude,reservable,price,place_type,review_average,number_of_reviews,saved);
 
-LOAD DATA INFILE 'images.csv'
-INTO TABLE airbnb_a.images
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-(image_url,properties_id);
-
 LOAD DATA INFILE 'properties_detail.csv'
-INTO TABLE airbnb_a.detail
+INTO TABLE airbnb.detail
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'images.csv'
+INTO TABLE airbnb.images
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(image_url,properties_id);
 
 
 -- LOAD DATA INFILE '/Users/choi/Desktop/java/airbnb-03/be/src/main/resources/data/properties.csv'
