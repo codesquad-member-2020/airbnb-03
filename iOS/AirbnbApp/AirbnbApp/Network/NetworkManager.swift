8ad20e610d5d17affcb406c1a00b0a5b72c8c0d5
corykim0829 <corykim0829@gmail.com>
2020-06-03 05:10:53 +0900

    [iOS] refactor: NetworkManager
    - 함수 들여쓰기

--- iOS/AirbnbApp/AirbnbApp/Network/NetworkManager.swift
+++ iOS/AirbnbApp/AirbnbApp/Network/NetworkManager.swift
@@ -8,25 +8,24 @@ enum NetworkErrorCase {
 
 final class NetworkManager {
     
-    static func getResource<Parameters: Encodable>(from URL: URL,
-                            parameters: Parameters? = nil,
-                            encoder: ParameterEncoder = URLEncodedFormParameterEncoder.default,
-                            completionHandler: @escaping (Result<Data, AFError>) -> Void) {
-        AF.request(URL,
-                   parameters: parameters,
-                   encoder: encoder)
-            .responseData { (responseData) in
-                switch responseData.result {
-                case .success(let data):
-                    completionHandler(.success(data))
-                case .failure(let error):
-                    completionHandler(.failure(error))
-                }
+    static func getResource<Parameters: Encodable>(
+        from url: URL,
+        parameters: Parameters? = nil,
+        encoder: ParameterEncoder = URLEncodedFormParameterEncoder.default,
+        completionHandler: @escaping (Result<Data, AFError>) -> Void) {
+        AF.request(url, parameters: parameters, encoder: encoder).responseData { (responseData) in
+            switch responseData.result {
+            case .success(let data):
+                completionHandler(.success(data))
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
         }
     }
     
-    static func getResource(from urlString: String,
-                           completionHandler: @escaping (Result<Data, AFError>) -> Void) {
+    static func getResource(
+        from urlString: String,
+        completionHandler: @escaping (Result<Data, AFError>) -> Void) {
         let url = URL(string: urlString)!
         AF.request(url).responseData { (response) in
             switch response.result {
