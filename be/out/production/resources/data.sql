dbd996101868dac265383a48645eeab269a53e01
Hamill210 <han95210@naver.com>
2020-06-05 05:55:23 +0900

    [BE] fix : 잘못된 도메인 주소 변경

--- be/out/production/resources/data.sql
+++ be/out/production/resources/data.sql
@@ -1,47 +1,47 @@
-LOAD DATA LOCAL INFILE '/home/ubuntu/properties.csv'
-INTO TABLE airbnb.properties
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(id,host_type,title,city,state,latitude,longitude,reservable,price,place_type,review_average,number_of_reviews,saved);
-
-LOAD DATA LOCAL INFILE '/home/ubuntu/properties_detail.csv'
-INTO TABLE airbnb.detail
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE '/home/ubuntu/images.csv'
-INTO TABLE airbnb.images
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-(image_url,properties_id);
-
-
-# LOAD DATA INFILE 'properties.csv'
-# INTO TABLE airbnb_a.properties
+# LOAD DATA LOCAL INFILE '/home/ubuntu/properties.csv'
+# INTO TABLE airbnb.properties
 # FIELDS TERMINATED BY ','
 # ENCLOSED BY '"'
 # LINES TERMINATED BY '\n'
 # IGNORE 1 ROWS
 # (id,host_type,title,city,state,latitude,longitude,reservable,price,place_type,review_average,number_of_reviews,saved);
 #
-# LOAD DATA INFILE 'properties_detail.csv'
-# INTO TABLE airbnb_a.detail
+# LOAD DATA LOCAL INFILE '/home/ubuntu/properties_detail.csv'
+# INTO TABLE airbnb.detail
 # FIELDS TERMINATED BY ','
 # ENCLOSED BY '"'
 # LINES TERMINATED BY '\n'
 # IGNORE 1 ROWS;
 #
-# LOAD DATA INFILE 'images.csv'
-# INTO TABLE airbnb_a.images
+# LOAD DATA LOCAL INFILE '/home/ubuntu/images.csv'
+# INTO TABLE airbnb.images
 # FIELDS TERMINATED BY ','
 # ENCLOSED BY '"'
 # LINES TERMINATED BY '\n'
 # (image_url,properties_id);
+
+
+LOAD DATA INFILE 'properties.csv'
+    INTO TABLE airbnb.properties
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS
+(id,host_type,title,city,state,latitude,longitude,reservable,price,place_type,review_average,number_of_reviews,saved);
+
+LOAD DATA INFILE 'properties_detail.csv'
+    INTO TABLE airbnb.detail
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'images.csv'
+    INTO TABLE airbnb.images
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    (image_url,properties_id);
 
 --
 -- INSERT INTO user(id, name, email) values (0001,'Alex','Alex@email.com');
--- be/src/main/resources/data.sql
+++ be/src/main/resources/data.sql
@@ -22,26 +22,26 @@ LINES TERMINATED BY '\n'
 
 
 # LOAD DATA INFILE 'properties.csv'
-# INTO TABLE airbnb_a.properties
-# FIELDS TERMINATED BY ','
-# ENCLOSED BY '"'
-# LINES TERMINATED BY '\n'
-# IGNORE 1 ROWS
+#     INTO TABLE airbnb.properties
+#     FIELDS TERMINATED BY ','
+#     ENCLOSED BY '"'
+#     LINES TERMINATED BY '\n'
+#     IGNORE 1 ROWS
 # (id,host_type,title,city,state,latitude,longitude,reservable,price,place_type,review_average,number_of_reviews,saved);
 #
 # LOAD DATA INFILE 'properties_detail.csv'
-# INTO TABLE airbnb_a.detail
-# FIELDS TERMINATED BY ','
-# ENCLOSED BY '"'
-# LINES TERMINATED BY '\n'
-# IGNORE 1 ROWS;
+#     INTO TABLE airbnb.detail
+#     FIELDS TERMINATED BY ','
+#     ENCLOSED BY '"'
+#     LINES TERMINATED BY '\n'
+#     IGNORE 1 ROWS;
 #
 # LOAD DATA INFILE 'images.csv'
-# INTO TABLE airbnb_a.images
-# FIELDS TERMINATED BY ','
-# ENCLOSED BY '"'
-# LINES TERMINATED BY '\n'
-# (image_url,properties_id);
+#     INTO TABLE airbnb.images
+#     FIELDS TERMINATED BY ','
+#     ENCLOSED BY '"'
+#     LINES TERMINATED BY '\n'
+#     (image_url,properties_id);
 
 --
 -- INSERT INTO user(id, name, email) values (0001,'Alex','Alex@email.com');
