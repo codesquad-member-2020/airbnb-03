a3364f7d937f53ae81a457cce2be23200f054a08
kjoonk <kjoonk@gmail.com>
2020-06-05 06:03:28 +0900

    [iOS] feat: Section Map 구현
    - 주소 및 설명 추가 및 데이터 표시
    - more 버튼 추가
    
    issue: #120

--- iOS/AirbnbApp/AirbnbApp/StayDetail/Sections/Views/SectionMap.swift
+++ iOS/AirbnbApp/AirbnbApp/StayDetail/Sections/Views/SectionMap.swift
@@ -1,11 +1,20 @@
 import UIKit
 import MapKit
 
-class SectionMap: ContentView {
+class SectionMap: ContentView, RoundedBorder {
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
 
-    func update(with annotation: MKPointAnnotation) {
-        mapView.delegate = self
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        drawBorders(cornerRadius: 12, borderWidth: 0.1, borderColor: UIColor.white.cgColor)
+        clipsToBounds = true
+    }
+
+    func updateMap(with annotation: MKPointAnnotation) {
+//        mapView.delegate = self
 
         mapView.isZoomEnabled = false
         mapView.isScrollEnabled = false
@@ -16,15 +25,15 @@ class SectionMap: ContentView {
     }
 }
 
-extension SectionMap: MKMapViewDelegate {
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        let annotationView = MKAnnotationView()
-        let imageView = UIImageView(image: UIImage(systemName: "house.fill"))
-        annotationView.addSubview(imageView)
-
-        return annotationView
-    }
-}
+//extension SectionMap: MKMapViewDelegate {
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        let annotationView = MKAnnotationView()
+//        let imageView = UIImageView(image: UIImage(systemName: "house.fill"))
+//        annotationView.addSubview(imageView)
+//
+//        return annotationView
+//    }
+//}
 
 struct MapInfo {
     let address: String
@@ -49,7 +58,10 @@ class SectionMapFactory {
 
         let annotation = MKPointAnnotation()
         annotation.coordinate = CLLocationCoordinate2D(latitude: mapInfo.latitude, longitude: mapInfo.longitude)
-        view.update(with: annotation)
+        view.updateMap(with: annotation)
+
+        view.addressLabel.text = mapInfo.address
+        view.descriptionLabel.text = mapInfo.description
 
         return view
     }
